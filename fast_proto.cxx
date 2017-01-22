11c8e3ac4262ec26d39724ef72ef3193dd25941d
wcout <wcout@users.noreply.github.com>
2017-01-22 09:32:55 +0100

    +comments

--- fast_proto.cxx
+++ fast_proto.cxx
@@ -40,8 +40,8 @@ void focus_cb( void *v_ )
 	if ( win )
 	{
 		static int state = 0;
-//		printf( "take focus\n" );
 		state++;
+		// do we need/have the cursor focus already?
 		if ( win && regain_focus && Fl::focus() == editor )
 		{
 			regain_focus = false;
@@ -49,10 +49,11 @@ void focus_cb( void *v_ )
 		}
 		if ( regain_focus )
 		{
+			// try to set focus to editor
 			win->show();
 			win->take_focus();
 		}
-		// cursor blinking
+		// als handle cursor blinking here..
 		if ( editor )
 			editor->show_cursor( state % 2 );
 		Fl::add_timeout( 0.2, focus_cb );
@@ -61,7 +62,6 @@ void focus_cb( void *v_ )
 
 int parse_first_error( int &line_, string& err_, string errfile_ )
 {
-//	printf( "parse_first_error\n" );
 	line_ = 0;
 	err_.erase();
 	stringstream ifs;
@@ -69,16 +69,15 @@ int parse_first_error( int &line_, string& err_, string errfile_ )
 	string buf;
 	while ( getline( ifs, buf ) )
 	{
-//		printf( "parsing: '%s'\n", buf.c_str() );
+		// parse for line with error
 		size_t errpos;
 		if ( ( errpos = buf.find( temp_cxx ) ) == 0 )
 		{
 			errpos += temp_cxx.size() + 1;
-//			printf( "errpos %u\n", errpos );
+			// found potential error line - line number is afterwards
 			line_ = atoi( buf.substr( errpos ).c_str() );
-//			printf( "error line: %d\n", line_ );
 			err_ = buf;
-			if ( line_ )
+			if ( line_ ) // if error line found, we are finished
 				break;
 		}
 	}
@@ -87,6 +86,7 @@ int parse_first_error( int &line_, string& err_, string errfile_ )
 
 string run_cmd( const string& cmd_ )
 {
+	// run a shell command and collect its output
 	FILE *f = popen( cmd_.c_str(), "r" );
 	string result;
 	if ( f )
@@ -101,6 +101,7 @@ string run_cmd( const string& cmd_ )
 
 pid_t execute( const char *exe_ )
 {
+//	printf( "execute '%s'\n", exe_ );
 #ifdef WIN32
 	pid_t child_pid = -1;
 	PROCESS_INFORMATION pi = { 0 };
@@ -124,17 +125,21 @@ pid_t execute( const char *exe_ )
 	{
 		int status;
 		waitpid( child_pid, &status, WNOHANG );
-		Fl::remove_timeout( focus_cb );
-		Fl::add_timeout( 0.2, focus_cb );
-		regain_focus = true;
 	}
 	else if ( child_pid == 0 )
 	{
-//		printf( "execute '%s'\n", exe_ );
 		execlp( exe_, exe_, NULL );
 		_exit( EXIT_FAILURE );
 	}
 #endif
+	if ( child_pid >  0 )
+	{
+		// we possibly have lost cursor focus,
+		// try to regain it..
+		Fl::remove_timeout( focus_cb );
+		Fl::add_timeout( 0.2, focus_cb );
+		regain_focus = true;
+	}
 	return child_pid;
 }
 
@@ -149,23 +154,29 @@ void terminate( pid_t pid_ )
 
 void compile_and_run( string code_ )
 {
-//	printf( "compile and run: '%s'\n", code_.c_str() );
+	//  write code to temp file
 	ofstream outf( temp_cxx.c_str() );
 	outf << code_;
 	outf.close();
+
+	// remove exe file
 	remove( temp.c_str() );
 	string cmd( compile_cmd + " " + temp_cxx + " 2>&1" );
+
+	// compile..
 	string result = run_cmd( cmd );
-//	printf( "Compile '%s' FILE = %p\n", cmd.c_str(), f );
-//	printf( "result: '%s'\n", result.c_str());
+
+//	printf( "Compile '%s' => '%s'\n", cmd.c_str(), result.c_str() );
 	if ( result.size() )
 	{
+		// there may have been compile errors..
 		int line;
 		string err;
 		parse_first_error( line, err, result );
 		if ( line )
 		{
-//			printf( "(line %d): %s\n", line, err.c_str());
+			// display error line in error panel
+//			printf( "error in line %d: %s\n", line, err.c_str() );
 			int start = textbuff->skip_lines( 0, line - 1 );
 			int end = textbuff->skip_lines( start, 1 );
 			textbuff->highlight( start, end );
@@ -180,7 +191,7 @@ void compile_and_run( string code_ )
 			errorbox->color( FL_GRAY );
 		}
 	}
-	// run only if exe changed
+	// re-run only if exe changed
 	if ( access( temp.c_str(), R_OK ) == 0 )
 	{
 		string cmd = changed_cmd + " " + temp;
@@ -201,6 +212,7 @@ void compile_and_run( string code_ )
 
 void cb_compile( void *v_ )
 {
+	// compile the source an re-run the target
 	Fl_Text_Buffer *buf = (Fl_Text_Buffer *)v_;
 	char *t = buf->text();
 	compile_and_run( t );
@@ -211,6 +223,9 @@ void changed_cb( int, int nInserted_, int nDeleted_, int, const char*, void* v_ 
 {
 	if ( nInserted_ || nDeleted_ )
 	{
+		// text in editor has been changed
+		// => prepare to compile source if no other changes are
+		//    made within the next 0.3 seconds
 		Fl::remove_timeout( cb_compile, v_ );
 		Fl::add_timeout( 0.3, cb_compile, v_ );
 	}
@@ -238,6 +253,7 @@ static int kf_duplicate_line( int c_, Fl_Text_Editor *e_ )
 
 int main( int argc_, char *argv_[] )
 {
+	// read in configurable values
 	Fl_Preferences cfg( ".", NULL, "fltk_fast_proto" );
 	int x, y, w, h;
 	cfg.get( "x", x, 100 );
@@ -249,6 +265,8 @@ int main( int argc_, char *argv_[] )
 	compile_cmd = text;
 	cfg.get( "changed_cmd", text, "shasum" );
 	changed_cmd = text;
+
+	// check if a source file is given
 	if ( argc_ > 1 )
 	{
 		string f = argv_[ 1 ];
@@ -260,12 +278,14 @@ int main( int argc_, char *argv_[] )
 				ext_pos = f.find( ".cpp" );
 			if ( ext_pos != string::npos )
 			{
+				// use default filename
 				temp_cxx = f;
 				temp = "./" + (string)fl_filename_name( f.substr( 0, ext_pos ).c_str() );
 			}
 		}
 	}
 
+	// create the editor window using last size
 	win = new Fl_Double_Window( w, h, "Fast FLTK prototyping" );
 	textbuff = new Fl_Text_Buffer();
 	editor = new Fl_Text_Editor( 0, 0, win->w(), win->h() - 30 );
@@ -275,13 +295,15 @@ int main( int argc_, char *argv_[] )
 	editor->textcolor( FL_WHITE );
 	editor->cursor_style( Fl_Text_Editor::SIMPLE_CURSOR );
 	editor->cursor_color( FL_GREEN );
-	editor->linenumber_width( 50 );
+	editor->linenumber_width( 50 ); // show line numbers
 	editor->textfont( FL_COURIER );
 	int ts;
 	cfg.get( "ts", ts, 14 );
 	editor->textsize( ts );
 	editor->linenumber_size( ts );
-	editor->buffer( textbuff ); // attach text buffer to editorlay widget
+	editor->buffer( textbuff ); // attach text buffer to editor
+
+	// add some key shortcuts (line deletion/duplication)
 	editor->add_key_binding( 'y', FL_CTRL, kf_delete_line );
 	editor->add_key_binding( 'd', FL_CTRL, kf_delete_line );
 	editor->add_key_binding( 'l', FL_CTRL, kf_duplicate_line );
@@ -291,29 +313,37 @@ int main( int argc_, char *argv_[] )
 	textbuff->tab_distance( 3 );
 	if ( textbuff->loadfile( temp_cxx.c_str() ) )
 	{
+		// a new source file is started
 		textbuff->text( "// type FLTK program here..\n" );
 		textbuff->select( 0, textbuff->length() );
 	}
 	else
 	{
+		// make a "backup" of the original source
 		string backup_file( temp_cxx + ".orig" );
 		textbuff->outputfile( backup_file.c_str(), 0, textbuff->length() );
 	}
+
+	// position cursor at end of file
 	editor->insert_position( textbuff->length() );
+
 	win->end();
 	win->resizable( win );
 	win->show();
-	win->position( x, y );
+	win->position( x, y );	// try to show window on last position
 
+	// show source filename in title
 	string title( temp_cxx + " - " + win->label() );
 	win->copy_label( title.c_str() );
 
+	// enter the main loop
 	Fl::run();
 
+	// stop a running program
 	if ( child_pid > 0 )
 		terminate( child_pid );
 
-
+	// save prefences
 	cfg.set( "x", win->x() );
 	cfg.set( "y", win->y() );
 	cfg.set( "w", win->w() );
